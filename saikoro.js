47a972ee9bcf36d83c4d69ad502674ecb6b3818f
student200036 <200036ww@gmail.com>
2022-02-28 14:00:50 +0900

    commit

--- saikoro.js
+++
@@ -1,7 +0,0 @@
-function roll_the_dice() {
-
-	let saikoro = Math.floor( Math.random() * 6) +1;//ランダムで１～６の数字を作ります
-	document.getElementById("saikoro_me").innerHTML = saikoro;//結果をid="kekka"に表示します
-    //document.getElementById("sainome").src= saikoro + ".jpg";//画像をid="sainome"に表示します
-
-}
